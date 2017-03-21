a71b1864cf203467e76d001d506314170d4e974c
Mikhail Collins <kyleryker@gmail.com>
2017-03-21 15:26:15 -0600

    first node project

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var messages = [{}];
+var username = '';
 
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json);
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({messages: messages});
+});
+
+app.post('/messages', function(req, res, next){
+  messages.push({message: req.body.message, time: new Date()}, username: req.body.username);
+  res.status(200).json({messages: messages});
+});
+
+app.listen(3000);
